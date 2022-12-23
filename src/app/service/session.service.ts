37b6a81451c28a8e8520dfcc30a420a755c65f8a
nourhaine1 <ayadinourhaine@gmail.com>
2022-12-23 20:18:14 +0100

    add imports

--- src/app/service/session.service.ts
+++ src/app/service/session.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Session } from '../api/session.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  public BaseUrl = '/FORMATION-SERVER/sessions';
+addData(s:Session ):Observable<any> {
+{
+    const headers = { 'content-type': 'application/json'}  
+    const body=JSON.stringify(s);
+    console.log(body)
+    return this.http.post(this.BaseUrl, body,{'headers':headers})
+  }
+}
 }
